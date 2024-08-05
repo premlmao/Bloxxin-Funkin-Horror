144495169b304103ba333e4607d8ab909af5d8cc
Prem1um <premchetandunna92714@gmail.com>
2024-08-05 17:11:05 +0530

    FINALLY CATBOY IS GONE (how tf do i fix DeformedOld bruh)

--- example_mods/custom_events/catboydeath.lua
+++
@@ -1,6 +0,0 @@
-function onCreate()
-	setPropertyFromClass('GameOverSubstate', 'characterName', 'bffurrydead'); --Character json file for the death animation
-	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'death'); --put in mods/sounds/
-	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'contentDeleted'); --put in mods/music/
-	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'kerplunk'); --put in mods/music/
-end
