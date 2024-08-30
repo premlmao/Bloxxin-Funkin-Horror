77a628bb5c151f49ece2ae838ddcacdd65f9ddc2
Prem1um <premchetandunna92714@gmail.com>
2024-08-31 00:30:24 +0530

    Fixing part 1

--- assets/shared/data/Succed/events.lua
+++
@@ -1,7 +0,0 @@
-function onCreate()
-
-makeLuaSprite('black',nil,0,0) makeGraphic('black',0xff00000,screenWidth*2,screenHeight*2)  screenCenter('black') addLuaSprite('black',true)
-end function onSongStart()
-doTweenAlpha('silly','black',0,5)
-
-end
