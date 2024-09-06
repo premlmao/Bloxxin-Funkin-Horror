cffcab014026f86c23cdf9feeb7fb127a48e830d
Prem1um <premchetandunna92714@gmail.com>
2024-09-07 00:14:28 +0530

    mostly finished rabbithole events (it means it's WIP ^_^)

---
+++ assets/shared/data/RabbitHole/vignette.lua
@@ -0,0 +1,19 @@
+function onCreate()
+
+    makeLuaSprite('vignette', 'vignette',0,0)
+    setObjectCamera('vignette', 'other')
+    setScrollFactor('vignette', 0, 0)
+    addLuaSprite('vignette', true)
+    setProperty('vignette.alpha', 0)
+
+end
+
+function onBeatHit()
+    if curBeat == 158 then
+        noteTweenAlpha('alphaNotes1', 0, 0, 1, 'linear');
+        noteTweenAlpha('alphaNotes2', 1, 0, 1, 'linear');
+        noteTweenAlpha('alphaNotes3', 2, 0, 1, 'linear');
+        noteTweenAlpha('alphaNotes4', 3, 0, 1, 'linear');
+        doTweenAlpha('viggyappears', 'vignette', 1, 0.5, 'linear')
+    end
+end
