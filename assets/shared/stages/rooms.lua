5873969b3090dc6a128562c507097ce7aa975cdc
Unfunny2 <jackstinson112@gmail.com>
2024-09-11 18:40:56 +0100

    THis might be a mistake
    uhhh

--- assets/shared/stages/rooms.lua
+++ assets/shared/stages/rooms.lua
@@ -1,18 +1,23 @@
 function onCreate()
 	-- background shit
 
-        makeAnimatedLuaSprite('rooms','stages/rooms',-180,180)addAnimationByPrefix                                    ('rooms','dance','run',20,true)
+        makeAnimatedLuaSprite('rooms','stages/rooms',-700,90)
+        addAnimationByPrefix('rooms','dance','run',20,true)
         objectPlayAnimation('rooms','dance',false)
         setScrollFactor('rooms', 1, 1);
+	scaleObject ('rooms', 2.5, 2.5);
+        setProperty('rooms.antialiasing', false);
 
-        makeAnimatedLuaSprite('bflegs','stages/bflegs',432,625)addAnimationByPrefix                                    ('bflegs','dance','run',24,true)
-        objectPlayAnimation('bflegs','dance',false)
-        setScrollFactor('bflegs', 1, 1);
+        makeAnimatedLuaSprite('robloxbflegs','stages/robloxbflegs',650,800)
+        addAnimationByPrefix('robloxbflegs','dance','run',20,true)
+        objectPlayAnimation('robloxbflegs','dance',false)
+        setScrollFactor('robloxbflegs', 1, 1);
+	scaleObject ('robloxbflegs', 2.5, 2.5);
+        setProperty('robloxbflegs.antialiasing', false);
 
 
         addLuaSprite('rooms', false);
-        addLuaSprite('bflegs', false);
-
+        addLuaSprite('robloxbflegs', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
