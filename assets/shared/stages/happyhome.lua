46aa136a4967a82141084267b108aeddaef2ebbc
Unfunny2 <jackstinson112@gmail.com>
2024-09-26 19:30:56 +0100

    f
    f

--- assets/shared/stages/happyhome.lua
+++ assets/shared/stages/happyhome.lua
@@ -1,19 +1,18 @@
 function onCreate()
 	-- background shit
 
+	makeLuaSprite('happyhometv', 'stages/happyhometv', -300, -100);
+	setScrollFactor('happyhometv', 1.2, 1.2);
 
-	makeLuaSprite('happyhome', 'stages/happyhome', -300, -75);
+	makeLuaSprite('happyhome', 'stages/happyhome', -300, -100);
 	setScrollFactor('happyhome', 1, 1);
-
-	makeLuaSprite('happyhomeback', 'stages/happyhomeback', -300, -100);
-	setScrollFactor('happyhomeback', 0.8, 0.8);
 
 	makeLuaSprite('night', 'stages/night', -300, -100);
 	setScrollFactor('night', 0.05, 0.05);
 
 
         addLuaSprite('night', false);
-        addLuaSprite('happyhomeback', false);
+        addLuaSprite('happyhometv', true);
         addLuaSprite('happyhome', false);
 end
 
