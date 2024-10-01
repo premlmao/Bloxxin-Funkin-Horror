fb671458630ef747705e5fd1b481e056d9b3cebc
Prem1um <premchetandunna92714@gmail.com>
2024-10-02 01:08:39 +0530

    succed now shakes when micheal screams | removed kenophobia events for future use

--- assets/shared/data/Succed/events.lua
+++ assets/shared/data/Succed/events.lua
@@ -1,7 +1,16 @@
 function onCreate()
+makeLuaSprite('black',nil,0,0) 
+makeGraphic('black',0xff00000,screenWidth*2,screenHeight*2)
+screenCenter('black') 
+addLuaSprite('black',true)
+end 
 
-makeLuaSprite('black',nil,0,0) makeGraphic('black',0xff00000,screenWidth*2,screenHeight*2)  screenCenter('black') addLuaSprite('black',true)
-end function onSongStart()
+function onSongStart()
 doTweenAlpha('silly','black',0,5)
+end
 
+function opponentNoteHit(_, _, type)
+    if type == 'Alt Animation' then
+        triggerEvent('Screen Shake', '0.1, 0.006,', '0.1, 0.004')
+    end
 end
