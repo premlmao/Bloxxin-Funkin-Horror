49d24e7912addf57f70bca613b9cc8665618dea1
Unfunny2 <jackstinson112@gmail.com>
2024-10-20 14:44:30 +0100

    THE BRICKBATTLE UPDATE.
    OH ITS PEAK.

--- assets/shared/stages/sfoth.lua
+++ assets/shared/stages/sfoth.lua
@@ -14,12 +14,9 @@ function onCreate()
 	scaleObject ('sfothrain', 1.75, 1.75);
         setProperty('sfothrain.antialiasing', false);
 
-        makeAnimatedLuaSprite('sfothbridge','stages/sfothbridge',-400,-250)
-        addAnimationByPrefix('sfothbridge','dance','splash',50,true)
-        objectPlayAnimation('sfothbridge','dance',false)
-        setScrollFactor('sfothbridge', 1, 1);
-	scaleObject ('sfothbridge', 1.75, 1.75);
-        setProperty('sfothbridge.antialiasing', false);
+        makeLuaSprite('sfothbridge2', 'stages/sfothbridge', -300, -100);
+	setScrollFactor('sfothbridge2', 1, 1);
+        setProperty('sfothbridge2.antialiasing', false);
 
         makeLuaSprite('sfothback', 'stages/sfothback', -300, -100);
 	setScrollFactor('sfothback', 0.2, 0.2);
@@ -33,5 +30,56 @@ function onCreate()
         addLuaSprite('sfothrain', true);
 	addLuaSprite('sfothback', false);
         addLuaSprite('sfothbridge', false);
-	addLuaSprite('sfothfog', false);
+	addLuaSprite('sfothfog', true);
+end
+
+function onBeatHit()
+    if curBeat == 462 then
+		removeLuaSprite('night');
+		removeLuaSprite('moon');
+                removeLuaSprite('sfothrain');
+		removeLuaSprite('sfothback');
+		removeLuaSprite('sfothbridge');
+		removeLuaSprite('sfothfog');
+
+        makeLuaSprite('night', 'stages/night', -300, -100);
+	setScrollFactor('night', 0.05, 0.05);
+        setProperty('night.antialiasing', false);
+
+        makeLuaSprite('mooncorrupted', 'stages/mooncorrupted', -300, -100);
+	setScrollFactor('mooncorrupted', 0.02, 0.02);
+        setProperty('mooncorrupted.antialiasing', false);
+
+        makeLuaSprite('sfothbridge2', 'stages/sfothbridge2', -300, -100);
+	setScrollFactor('sfothbridge2', 1, 1);
+        setProperty('sfothbridge2.antialiasing', false);
+
+        makeAnimatedLuaSprite('messanger','stages/messanger',400,280)
+        addAnimationByPrefix('messanger','dance','jiggy',20,true)
+        objectPlayAnimation('messanger','dance',false)
+        setScrollFactor('messanger', 1, 1);
+	scaleObject ('messanger', 1.1, 1.1);
+        setProperty('messanger.antialiasing', false);
+
+        makeAnimatedLuaSprite('messanger2','stages/messanger2',1200,330)
+        addAnimationByPrefix('messanger2','dance','jiggy',20,true)
+        objectPlayAnimation('messanger2','dance',false)
+        setScrollFactor('messanger2', 1, 1);
+	scaleObject ('messanger2', 1.2, 1.2);
+        setProperty('messanger2.antialiasing', false);
+
+        makeAnimatedLuaSprite('messanger3','stages/messanger3',500,200)
+        addAnimationByPrefix('messanger3','dance','jiggy',20,true)
+        objectPlayAnimation('messanger3','dance',false)
+        setScrollFactor('messanger3', 1, 1);
+	scaleObject ('messanger3', 1.2, 1.2);
+        setProperty('messanger3.antialiasing', false);
+
+	addLuaSprite('night', false);
+	addLuaSprite('mooncorrupted', false);
+        addLuaSprite('sfothbridge2', false);
+	addLuaSprite('messanger', false);
+	addLuaSprite('messanger2', false);
+	addLuaSprite('messanger3', false);
+end
 end
