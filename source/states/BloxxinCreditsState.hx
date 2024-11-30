f34c5d7ff7d925a3b829491cb68dcc36afada6fb
Prem1um <premchetandunna92714@gmail.com>
2024-11-30 22:12:36 +0530

    useless ass update

--- source/states/BloxxinCreditsState.hx
+++ source/states/BloxxinCreditsState.hx
@@ -1,26 +1,40 @@
 package states;
 
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxBackdrop;
+import flixel.addons.display.FlxGridOverlay;
 import flixel.addons.transition.FlxTransitionableState;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.tweens.misc.NumTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import lime.utils.Assets;
 import objects.AttachedSprite;
 
 class BloxxinCreditsState extends MusicBeatState
 {
 	var curSelected:Int = -1;
 
-	private var grpOptions:FlxTypedGroup<Alphabet>;
-	private var iconArray:Array<AttachedSprite> = [];
-	private var creditsStuff:Array<Array<String>> = [];
+	var offsetThing:Float = -75;
+
+	var teamList:Array<Array<String>> = [
+		'prem',
+		'nil',
+		'unfunny2',
+		'placeholder',
+	];
 
 	var bg:FlxSprite;
 	var baseplate:FlxSprite;
-	var character:FlxSprite;
-	var descText:FlxText;
-	var intendedColor:FlxColor;
-	var colorTween:FlxTween;
-	var descBox:AttachedSprite;
 	var wipText:FlxText;
-
-	var offsetThing:Float = -75;
+	var member:FlxSprite;
 
 	override function create()
 	{
@@ -34,7 +48,6 @@ class BloxxinCreditsState extends MusicBeatState
 		if (!FlxG.mouse.visible)
             FlxG.mouse.visible = true;
 
-
 		persistentUpdate = true;
 		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		bg.antialiasing = ClientPrefs.data.antialiasing;
@@ -45,14 +58,6 @@ class BloxxinCreditsState extends MusicBeatState
 		baseplate.antialiasing = ClientPrefs.data.antialiasing;
 		add(baseplate);
 		baseplate.screenCenter();
-
-		character = new FlxSprite().loadGraphic(Paths.image('credits/Bloxxin/character_placeholder'));
-		character.antialiasing = ClientPrefs.data.antialiasing;
-		character.scale.x = 1.5;
-		character.scale.y = 1.5;
-		character.x = 100;
-		character.y = 250;
-		add(character);
 
 		wipText = new FlxText(0, 0, FlxG.width,
 			"WORK IN\n
@@ -66,36 +71,19 @@ class BloxxinCreditsState extends MusicBeatState
 		wipText.x = 300;
 		add(wipText);
 
-		grpOptions = new FlxTypedGroup<Alphabet>();
-		add(grpOptions);
+		var members:FlxTypedGroup<FlxSprite>;
+		members = new FlxTypedGroup<FlxSprite>();
+		add(members);
 
-		var defaultList:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
-			['Psych Engine Team'],
-			['Shadow Mario',		'shadowmario',		'Main Programmer and Head of Psych Engine',					 'https://ko-fi.com/shadowmario',		'444444'],
-			['Riveren',				'riveren',			'Main Artist/Animator of Psych Engine',						 'https://twitter.com/riverennn',		'14967B'],
-			[''],
-			['Former Engine Members'],
-			['bb-panzu',			'bb',				'Ex-Programmer of Psych Engine',							 'https://twitter.com/bbsub3',			'3E813A'],
-			['shubs',				'',					'Ex-Programmer of Psych Engine\nI don\'t support them.',	 '',									'A1A1A1'],
-			[''],
-			['Engine Contributors'],
-			['CrowPlexus',			'crowplexus',		'Input System v3, Major Help and Other PRs',				 'https://twitter.com/crowplexus',		'A1A1A1'],
-			['Keoiki',				'keoiki',			'Note Splash Animations and Latin Alphabet',				 'https://twitter.com/Keoiki_',			'D2D2D2'],
-			['SqirraRNG',			'sqirra',			'Crash Handler and Base code for\nChart Editor\'s Waveform', 'https://twitter.com/gedehari',		'E1843A'],
-			['EliteMasterEric',		'mastereric',		'Runtime Shaders support',									 'https://twitter.com/EliteMasterEric',	'FFBD40'],
-			['PolybiusProxy',		'proxy',			'.MP4 Video Loader Library (hxCodec)',						 'https://twitter.com/polybiusproxy',	'DCD294'],
-			['Tahir',				'tahir',			'Implementing & Maintaining SScript and Other PRs',			 'https://twitter.com/tahirk618',		'A04397'],
-			['iFlicky',				'flicky',			'Composer of Psync and Tea Time\nMade the Dialogue Sounds',	 'https://twitter.com/flicky_i',		'9E29CF'],
-			['KadeDev',				'kade',				'Fixed some issues on Chart Editor and Other PRs',			 'https://twitter.com/kade0912',		'64A250'],
-			['superpowers04',		'superpowers04',	'LUA JIT Fork',												 'https://twitter.com/superpowers04',	'B957ED'],
-			['CheemsAndFriends',	'face',	'Creator of FlxAnimate\n(Icon will be added later, merry christmas!)',	 'https://twitter.com/CheemsnFriendos',	'A1A1A1'],
-			[''],
-			["Funkin' Crew"],
-			['ninjamuffin99',		'ninjamuffin99',	"Programmer of Friday Night Funkin'",						 'https://twitter.com/ninja_muffin99',	'CF2D2D'],
-			['PhantomArcade',		'phantomarcade',	"Animator of Friday Night Funkin'",							 'https://twitter.com/PhantomArcade3K',	'FADC45'],
-			['evilsk8r',			'evilsk8r',			"Artist of Friday Night Funkin'",							 'https://twitter.com/evilsk8r',		'5ABD4B'],
-			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",							 'https://twitter.com/kawaisprite',		'378FC7']
-		];
+		for (i in 0...teamList.length) {
+			member = new FlxSprite().loadGraphic(Paths.image('credits/Bloxxin/member_' + teamList[i]));
+			member.antialiasing = ClientPrefs.data.antialiasing;
+			member.scale.set(1.5, 1.5);
+			member.x = 100;
+			member.y = 250;
+			member.ID = i;
+		}
+		members.add(member);
 
 		super.create();
 	}
