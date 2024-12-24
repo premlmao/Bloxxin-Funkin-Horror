655cf805e7830e4e432e9a1e96935063bfa5ea5e
Prem1um <premchetandunna92714@gmail.com>
2024-12-25 01:48:04 +0530

    Update setup-windows.bat

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -3,15 +3,22 @@ color 0a
 cd ..
 @echo on
 echo Installing dependencies.
-haxelib install lime
-haxelib install openfl
-haxelib install flixel
-haxelib install flixel-addons
-haxelib install flixel-ui
-haxelib install flixel-tools
-haxelib install SScript
-haxelib install hxCodec
-haxelib install tjson
+haxelib install lime 8.2.1
+haxelib set lime 8.2.1
+haxelib install openfl 9.4.0
+haxelib set openfl 9.4.0
+haxelib install flixel 5.5.0
+haxelib set flixel 5.5.0
+haxelib install flixel-addons 3.2.3
+haxelib set flixel-addons 3.2.3
+haxelib install flixel-ui 2.6.1
+haxelib set flixel-ui 2.6.1
+haxelib install flixel-tools 1.5.1
+haxelib set flixel-tools 1.5.1
+haxelib install hxCodec 3.0.2
+haxelib set hxCodec 3.0.2
+haxelib install tjson 1.4.0
+haxelib set tjson 1.4.0
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
