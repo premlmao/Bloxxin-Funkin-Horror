6a3374f59008e05cde427a49cfcc47fff7157ad0
Unfunny2 <jackstinson112@gmail.com>
2024-12-29 19:19:24 +0000

    q

--- assets/shared/stages/pof.lua
+++ assets/shared/stages/pof.lua
@@ -5,40 +5,11 @@ function onCreate()
 	setScrollFactor('sky', 0.05, 0.05);
         setProperty('sky.antialiasing', false);
 
-        makeLuaSprite('poffog', 'stages/poffog', -300, -100);
-	setScrollFactor('poffog', 0.5, 0.5);
-
 	makeLuaSprite('pofplate', 'stages/pofplate', -300, -100);
 	setScrollFactor('pofplate', 1, 1);
         setProperty('pofplate.antialiasing', false);
 
-	makeAnimatedLuaSprite('pofplayers','stages/pofplayers',-200,-100)addAnimationByPrefix('pofplayers', 'grin', 'bop', 24, false);
-	setScrollFactor('pofplayers', 1, 1);
-	scaleObject ('pofplayers', 1.75, 1.75);
-        setProperty('pofplayers.antialiasing', false);
-
-        makeLuaSprite('pofmines', 'stages/pofmines', -300, -100);
-	setScrollFactor('pofmines', 1, 1);
-        setProperty('pofmines.antialiasing', false);
-
-        makeLuaSprite('pofminesback', 'stages/pofminesback', -300, -100);
-	setScrollFactor('pofminesback', 0.95, 0.95);
-        setProperty('pofminesback.antialiasing', false);
-
-        makeLuaSprite('pofminesbacker', 'stages/pofminesbacker', -300, -100);
-	setScrollFactor('pofminesbacker', 0.85, 0.85);
-        setProperty('pofminesbacker.antialiasing', false);
-
 
 	addLuaSprite('sky', false);
-        addLuaSprite('poffog', true);
         addLuaSprite('pofplate', false);
-	addLuaSprite('pofplayers', false);
-        addLuaSprite('pofmines', false);
-        addLuaSprite('pofminesback', false);
-        addLuaSprite('pofminesbacker', false);
-end
-
-function onBeatHit()
-	objectPlayAnimation('pofplayers','grin',true)
 end
