c348f80bf7f4830289f7fba5f79ed22566cafdc7
Unfunny2 <jackstinson112@gmail.com>
2025-03-08 18:16:05 +0000

    msmorgasboard

--- assets/shared/stages/pof.lua
+++ assets/shared/stages/pof.lua
@@ -13,3 +13,33 @@ function onCreate()
 	addLuaSprite('sky', false);
         addLuaSprite('pofplate', false);
 end
+
+
+function onBeatHit()
+    if curBeat == 96 then
+		removeLuaSprite('sky');
+		removeLuaSprite('pofplate');
+
+        makeAnimatedLuaSprite('floatingpoint','stages/floatingpoint',-320,-120)
+        addAnimationByPrefix('floatingpoint','dance','glitch',24,true)
+        objectPlayAnimation('floatingpoint','dance',false)
+        setScrollFactor('floatingpoint', 0.05, 0.05);
+        setProperty('floatingpoint.antialiasing', false);
+
+	makeLuaSprite('pofplate', 'stages/pofplate', -300, -100);
+	setScrollFactor('pofplate', 1, 1);
+        setProperty('pofplate.antialiasing', false);
+
+        addLuaSprite('floatingpoint', false);
+        addLuaSprite('pofplate', false);
+end
+    if curBeat == 160 then
+
+	makeLuaSprite('pofmines', 'stages/pofmines', -300, -100);
+	setScrollFactor('pofmines', 1, 1);
+        setProperty('pofmines.antialiasing', false);
+
+        addLuaSprite('pofmines', false);
+end
+end
+
