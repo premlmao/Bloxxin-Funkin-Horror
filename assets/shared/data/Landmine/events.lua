1c341366f59720560654d328414bf6f9368bcd30
Prem1um <premchetandunna92714@gmail.com>
2025-03-09 22:15:17 +0530

    i need nil's help man

--- assets/shared/data/Landmine/events.lua
+++ assets/shared/data/Landmine/events.lua
@@ -10,6 +10,16 @@ function onCreate()
 end
 
 function onBeatHit()
+    function onUpdatePost()
+        if curBeat == 158 then
+        doTweenAlpha('byeHUD6', 'timeTxt', 0, crochet*0.006, 'cubeInOut')
+        doTweenAlpha('byeHUD7', 'songIcon', 0, crochet*0.006, 'cubeInOut')
+        end
+        if curBeat == 224 then
+            doTweenAlpha('byeHUD6', 'timeTxt', 1, crochet*0.006, 'cubeInOut')
+            doTweenAlpha('byeHUD7', 'songIcon', 1, crochet*0.006, 'cubeInOut')
+        end
+    end
     if curBeat == 159 then
         noteTweenX('noteX1', 0, 2000, 1, 'cubeOut')
         noteTweenX('noteX2', 1, 2000, 1, 'cubeOut')
@@ -31,6 +41,35 @@ function onBeatHit()
             doTweenY('byeHUD4','iconP2',-300,crochet*0.006,'cubeInOut')
             doTweenY('byeHUD5','scoreTxt',-300,crochet*0.006,'cubeInOut')
         end
+    end
+
+    if curBeat == 224 then
+        doTweenY('hiHUD1','healthBar',641,crochet*0.005,'cubeInOut')
+        doTweenY('hiHUD2','healthBarAround',629,crochet*0.005,'cubeInOut')
+        doTweenY('hiHUD3','iconP1',554,crochet*0.005,'cubeInOut')
+        doTweenY('hiHUD4','iconP2',554,crochet*0.005,'cubeInOut')
+        doTweenY('hiHUD5','scoreTxt',689,crochet*0.005,'cubeInOut')
+        doTweenAlpha('hiHUD6', 'timeBar',1,crochet*0.005,'linear')
+        if downscroll then
+            doTweenY('hiHUD1','healthBar',79,crochet*0.005,'cubeInOut')
+            doTweenY('hiHUD2','healthBarAround',67,crochet*0.005,'cubeInOut')
+            doTweenY('hiHUD3','iconP1',4,crochet*0.005,'cubeInOut')
+            doTweenY('hiHUD4','iconP2',4,crochet*0.005,'cubeInOut')
+            doTweenY('hiHUD5','scoreTxt',129,crochet*0.005,'cubeInOut')
+        end
+    end
+    if curBeat == 225 then
+        noteTweenX('noteX1', 0, 750, 1, 'cubeOut')
+        noteTweenX('noteX2', 1, 860, 1, 'cubeOut')
+        noteTweenX('noteX3', 2, 970, 1, 'cubeOut')
+        noteTweenX('noteX4', 3, 1080, 1, 'cubeOut')
+        noteTweenX('noteX5', 4, 100, 1, 'cubeOut')
+        noteTweenX('noteX6', 5, 210, 1, 'cubeOut')
+        noteTweenX('noteX7', 6, 320, 1, 'cubeOut')
+        noteTweenX('noteX8', 7, 430, 1, 'cubeOut')
+    end
+    if curBeat == 416 then
+        setProperty('camHUD.visible', false)
     end
     function opponentNoteHit()
         if curBeat > 160 and curBeat < 224 then
