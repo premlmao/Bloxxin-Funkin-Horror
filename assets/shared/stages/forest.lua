4aff7b753f8fedbf5c74080dd6544be90e4d0a59
Unfunny2 <jackstinson112@gmail.com>
2025-06-02 15:20:40 +0100

    check corroded

--- assets/shared/stages/forest.lua
+++ assets/shared/stages/forest.lua
@@ -1,45 +1,22 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('forest', 'stages/forest', -300, -100);
+	makeLuaSprite('forest', 'stages/forest', -1300, -750);
 	setScrollFactor('forest', 0.8, 0.8);
         setProperty('forest.antialiasing', false);
+	scaleObject ('forest', 3, 3);
 
-	makeLuaSprite('forestfloor', 'stages/forestfloor', -300, -100);
+	makeLuaSprite('forestfloor', 'stages/forestfloor', -1300, -730);
 	setScrollFactor('forestfloor', 1, 1);
         setProperty('forestfloor.antialiasing', false);
+	scaleObject ('forestfloor', 3, 3);
 
-	makeLuaSprite('foresttree', 'stages/foresttree', -300, -100);
+	makeLuaSprite('foresttree', 'stages/foresttree', -1300, -730);
 	setScrollFactor('foresttree', 1.7, 1.7);
         setProperty('foresttree.antialiasing', false);
-
-	makeLuaSprite('rustcomputer', 'stages/rustcomputer', -560, 1000);
-	setScrollFactor('rustcomputer', 0.8, 0.8);
-        setProperty('rustcomputer.antialiasing', false);
+	scaleObject ('foresttree', 3, 3);
 
 
 	addLuaSprite('foresttree', true);
 	addLuaSprite('forest', false);
 	addLuaSprite('forestfloor', false);
-	addLuaSprite('rustcomputer', false);
-	
-end
-
-if songName == 'CorrodedMetal' then
-function onBeatHit()
-    if curBeat == 177 then
-        doTweenY('computer', 'rustcomputer', -100, 1, 'cubeInOut')
-        doTweenY('forestf', 'forestfloor', -500, 1, 'cubeInOut')
-        doTweenY('forest', 'forest', -500, 1, 'cubeInOut')
-        doTweenY('tree', 'foresttree', -1000, 1, 'cubeInOut')
-        doTweenAlpha('boygrend', 'boyfriend', 0, 1, 'cubeInOut')
-end
-    if curBeat == 320 then
-	removeLuaSprite('rustcomputer', false);
-
-        doTweenAlpha('boygrend', 'boyfriend', 1, 0.01, 'cubeInOut')
-        doTweenY('forestf', 'forestfloor', -100, 0.01, 'cubeInOut')
-        doTweenY('forest', 'forest', -100, 0.01, 'cubeInOut')
-        doTweenY('tree', 'foresttree', -100, 0.01, 'cubeInOut')
-end
-end
 end
