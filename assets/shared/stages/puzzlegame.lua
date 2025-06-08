15f7bef3bbe0fdd29fc2a93b14a45398b9f621be
Unfunny2 <jackstinson112@gmail.com>
2025-06-08 11:13:01 +0100

    gloryday prep

--- assets/shared/stages/puzzlegame.lua
+++ assets/shared/stages/puzzlegame.lua
@@ -1,17 +1,20 @@
 function onCreate()
 	-- background shit
 
-	makeLuaSprite('puzzlegame', 'stages/puzzlegame', -300, -100);
+	makeLuaSprite('puzzlegame', 'stages/puzzlegame', -1300, -730);
 	setScrollFactor('puzzlegame', 1, 1);
         setProperty('puzzlegame.antialiasing', false);
+	scaleObject ('puzzlegame', 3, 3);
 
-	makeLuaSprite('moonyellow', 'stages/moonyellow', -300, -300);
+	makeLuaSprite('moonyellow', 'stages/moonyellow', -1300, -730);
 	setScrollFactor('moonyellow', 0.02, 0.02);
         setProperty('moonyellow.antialiasing', false);
+	scaleObject ('moonyellow', 3, 3);
 
-	makeLuaSprite('redsky', 'stages/redsky', -300, -100);
+	makeLuaSprite('redsky', 'stages/redsky', -1300, -750);
 	setScrollFactor('redsky', 0.05, 0.05);
         setProperty('redsky.antialiasing', false);
+	scaleObject ('redsky', 3, 3);
 
 
         addLuaSprite('redsky', false);
